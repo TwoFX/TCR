121c1062db1b47c774cf710186c4f05ccdcc2901
Markus Himmel <markus@lean-fro.org>
2025-10-16 14:34:48 +0200

    Fix definition of segment tree

--- TCR/Data/SegmentTree/Basic.lean
+++ TCR/Data/SegmentTree/Basic.lean
@@ -12,39 +12,48 @@ namespace TCR
 /-- A vector of size `2 * n` is an `op`-segment tree if, when interpreted as a binary tree,
 it satisfies the heap-like property that the parent is always the `op` of the children. -/
 structure IsSegmentTree (op : α → α → α) (neutral : α) (v : Vector α (2 * n)) : Prop where
-  /-- Given `i < n`, `v[i] = op v[2 * i + 1] v[2 * i + 2]`. -/
-  op_eq : ∀ (i : Nat) (hi : i < n), v[i] = op v[2 * i + 1] (v[(2 * i + 2)]?.getD neutral)
+  zero_eq : ∀ (h : 0 < 2 * n), v[0] = neutral
+  /-- Given `0 < i < n`, `v[i] = op v[2 * i + 1] v[2 * i + 2]`. -/
+  op_eq : ∀ (i : Nat) (_ : 0 < i) (hi : i < n), v[i] = op v[2 * i] (v[(2 * i + 1)]?.getD neutral)
 
 /-- Creates a new `op`-segment tree on the vector `v`. -/
 def mkSegmentTree (op : α → α → α) (neutral : α) (v : Vector α n) : Vector α (2 * n) :=
-  let vec := Vector.mk (Array.replicate n neutral ++ v.toArray) (by grind)
-  loop vec n (Nat.le_refl _)
+  if h : n = 0 then
+    Vector.mk #[] (by simp [h])
+  else
+    let vec := Vector.mk (Array.replicate n neutral ++ v.toArray) (by grind)
+    loop vec (n - 1) (by omega)
 where
-  loop (vec : Vector α (2 * n)) (idx : Nat) (hidx : idx ≤ n) : Vector α (2 * n) :=
+  loop (vec : Vector α (2 * n)) (idx : Nat) (hidx : idx < n) : Vector α (2 * n) :=
     match h : idx with
     | 0 => vec
-    | idx' + 1 => loop (vec.set idx' (op vec[2 * idx' + 1] (vec.getD (2 * idx' + 2) neutral))) idx' (by grind)
+    | idx' + 1 => loop (vec.set (idx' + 1) (op vec[2 * (idx' + 1)] (vec.getD (2 * (idx' + 1) + 1) neutral))) idx' (by grind)
 
 theorem isSegmentTree_mkSegmentTree {op : α → α → α} {neutral : α} {v : Vector α n} :
     IsSegmentTree op neutral (mkSegmentTree op neutral v) := by
   rw [mkSegmentTree]
-  exact loop _ _ (by grind)
+  split
+  · refine ⟨by simp_all, by simp_all⟩
+  · refine loop _ _ ?_ (by grind)
+    simp_all [Array.getElem_append]
 where
-  loop {vec : Vector α (2 * n)} (idx : Nat) (hidx : idx ≤ n)
-      (h : ∀ idx', idx ≤ idx' → (h : idx' < n) →
-        vec[idx'] = op vec[2 * idx' + 1] (vec.getD (2 * idx' + 2) neutral)) :
+  loop {vec : Vector α (2 * n)} (idx : Nat) (hidx : idx < n)
+      (h₀ : vec[0] = neutral)
+      (h : ∀ idx', 0 < idx' → idx < idx' → (h : idx' < n) →
+        vec[idx'] = op vec[2 * idx'] (vec.getD (2 * idx' + 1) neutral)) :
       IsSegmentTree op neutral (mkSegmentTree.loop op neutral vec idx hidx) := by
     fun_induction mkSegmentTree.loop with
-    | case1 => exact ⟨by simpa using h⟩
+    | case1 => exact ⟨by simp_all, by simpa using h⟩
     | case2 vec idx' hidx ih =>
-      refine ih (fun idx'' h₁ h₂ => ?_)
-      rw [Vector.getElem_set]
-      split
-      · rename_i h
-        subst h
-        rw [Vector.getElem_set_ne _ _ (by omega), Vector.getD_set_ne (by omega)]
-      · rw [Vector.getElem_set_ne _ _ (by omega), Vector.getD_set_ne (by omega)]
-        apply h <;> omega
+      refine ih ?_ (fun idx'' h₁ h₂ h₃ => ?_)
+      · rw [Vector.getElem_set_ne _ _ (by omega), h₀]
+      · rw [Vector.getElem_set]
+        split
+        · rename_i h
+          subst h
+          rw [Vector.getElem_set_ne _ _ (by omega), Vector.getD_set_ne (by omega)]
+        · rw [Vector.getElem_set_ne _ _ (by omega), Vector.getD_set_ne (by omega)]
+          apply h <;> omega
 
 /-- Given a segment tree, extracts the underlying data by copying it. -/
 def underlying (v : Vector α (2 * n)) : Vector α n :=
@@ -63,24 +72,33 @@ theorem getElem?_eq_getElem?_underlying {v : Vector α (2 * n)} {i : Nat} (hi' :
 @[simp]
 theorem underlying_mkSegmentTree {op : α → α → α} {neutral : α} {v : Vector α n} :
     underlying (mkSegmentTree op neutral v) = v := by
-  suffices ∀ (vec : Vector α (2 * n)) idx hidx, (mkSegmentTree.loop op neutral vec idx hidx).extract n (2 * n) = vec.extract n (2 * n) by
-    simp [underlying, mkSegmentTree, this]
+  rw [mkSegmentTree]
+  split
+  · simp_all [underlying]
+  suffices ∀ (vec : Vector α (2 * n)) idx hidx,
+      (mkSegmentTree.loop op neutral vec idx hidx).extract n (2 * n) = vec.extract n (2 * n) by
+    simp [underlying, this]
     grind
   intro vec idx hidx
   fun_induction mkSegmentTree.loop <;> grind
 
-theorem IsSegmentTree.underlying_inj (v v' : Vector α (2 * n)) (hv : IsSegmentTree op neutral v) (hv' : IsSegmentTree op neutral v')
+theorem IsSegmentTree.underlying_inj (v v' : Vector α (2 * n))
+    (hv : IsSegmentTree op neutral v) (hv' : IsSegmentTree op neutral v')
     (h : underlying v = underlying v') : v = v' := by
-  suffices ∀ i, (h : i < n) → v[n - i - 1] = v'[n - i - 1] by
+  suffices ∀ i, (h : i < n - 1) → v[n - i - 1] = v'[n - i - 1] by
     ext i hi
     have := this (n - i - 1)
+    have := hv.zero_eq
+    have := hv'.zero_eq
     grind [getElem_eq_getElem_underlying]
   intro i hi
   induction i using Nat.strongRecOn with
   | ind i ih =>
-    rw [hv.op_eq _ (by grind), hv'.op_eq _ (by grind)]
+    rw [hv.op_eq _ (by grind) (by grind), hv'.op_eq _ (by grind) (by grind)]
+    have := ih (2 * i + 1 - n)
     have := ih (2 * i - n)
-    have := ih (2 * i - 1 - n)
-    grind (splits := 10) [getElem_eq_getElem_underlying]
+    grind (splits := 12) [getElem_eq_getElem_underlying]
+
+
 
 end TCR
